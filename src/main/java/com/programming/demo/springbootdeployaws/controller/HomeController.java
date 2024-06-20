786e8e87f9e33360a903cce7535bf11d106cf94e
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-20 15:33:19 +0700

    update

--- src/main/java/com/programming/demo/springbootdeployaws/controller/HomeController.java
+++ src/main/java/com/programming/demo/springbootdeployaws/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.programming.demo.springbootdeployaws.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class HomeController {
     @GetMapping
     public String get() {
