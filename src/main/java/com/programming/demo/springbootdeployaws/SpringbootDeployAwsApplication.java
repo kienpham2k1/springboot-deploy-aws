44b8a56fb1ff23b534b0a9123a83ec2ba220f2f0
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-20 15:23:58 +0700

    deploy

---
+++ src/main/java/com/programming/demo/springbootdeployaws/SpringbootDeployAwsApplication.java
@@ -0,0 +1,13 @@
+package com.programming.demo.springbootdeployaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeployAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDeployAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programming/demo/springbootdeployaws/SpringbootDeployAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.demo.springbootdeployaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeployAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
