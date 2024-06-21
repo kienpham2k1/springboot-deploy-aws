1e7c27eba63b3d295e91cd16513dcfd98c15c73d
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-21 08:42:21 +0700

    sample bbean stalk env var

--- src/main/java/com/programming/demo/springbootdeployaws/controller/HomeController.java
+++ src/main/java/com/programming/demo/springbootdeployaws/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.programming.demo.springbootdeployaws.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+    @Value("${RDS_USERNAME}")
+    String userName;
     @GetMapping
     public String get() {
-        return "Hello ";
+        return "Hello "+ userName;
     }
 }
